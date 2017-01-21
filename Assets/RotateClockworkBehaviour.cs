2db9c9e08fb6657e4d7b7a9f4e3db7fc28cb63a7
David <David>
2017-01-20 23:05:49 -0500

    Dinky hallway scene

---
+++ Assets/RotateClockworkBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateClockworkBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine("ClockRotateNinety");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    IEnumerator ClockRotateNinety()
+    {
+        while (true)
+        {
+
+            Vector3 initialRot = transform.rotation.eulerAngles;
+            Vector3 targetRot = new Vector3(initialRot.x, initialRot.y, initialRot.z);
+            targetRot.z += 90;
+            targetRot.z %= 360;
+
+            Vector3 currentRot = new Vector3(initialRot.x, initialRot.y, initialRot.z);
+
+            for (float i = 0; i < 1; i += Time.deltaTime / 2.0f)
+            {
+                currentRot = Vector3.Lerp(initialRot, targetRot, i);
+                transform.localEulerAngles = currentRot;
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(2.0f);
+        }
+    }
+
+}

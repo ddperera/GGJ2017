f28e1bd00d7409d09afdba65d784b362b47e89eb
David <David>
2017-01-22 08:23:26 -0500

    time trial beginninggggggggggssssssssss

---
+++ Assets/TimeTrialManagerBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTrialManagerBehaviour : MonoBehaviour {
+
+    float timer;
+    bool timeTicking;
+	// Use this for initialization
+	void Start ()
+    {
+        timer = 0f;
+        timeTicking = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(timeTicking)
+        {
+            timer += Time.deltaTime;
+        }
+	}
+
+    private void OnGUI()
+    {
+
+    }
+
+    public void StartTime()
+    {
+        timeTicking = true;
+    }
+
+    public void StopTime()
+    {
+        timeTicking = false;
+    }
+}
